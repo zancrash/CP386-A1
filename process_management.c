6143af63ee9a8d7bde931a90e2c2b70d86579c19
zancrash <farzanali1296@gmail.com>
2021-06-01 01:57:38 -0400

    started a bit on q2, created child process to read file

--- process_management.c
+++ process_management.c
@@ -6,5 +6,17 @@
 #include <sys/stat.h>
 
 int main() {
+
+  char * line = NULL;
+
+  int pid = fork();
+
+  if (pid<0){
+    exit(1);
+  } else if (pid == 0){
+    FILE *fp = fopen("sample_in.txt", "r");
+    fscanf(fp, "%s", line);
+  }
+
   return 0;
 }
