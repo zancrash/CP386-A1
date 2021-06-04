2ce3eb933dd5634267ae4c9a67d93aab154aeb95
mklaso <mklasninovski@gmail.com>
2021-06-04 15:16:52 -0400

    setup splitting the shared memory string into separate commands and put them in the array.

--- process_management.c
+++ process_management.c
@@ -21,25 +21,17 @@ void writeOutput(char* command, char* output) {
 #define MAX_LINE_LEN 60
 
 int main() {
-  int SIZE = 4096;          // size in bytes of shared memory object
-  char* name = "OS";        // named of shared memory object
-  int shm_fd;               // shared memory file descriptor
-  void* mem_ptr;            // pointer to shared memory object
-  char line[MAX_LINE_LEN];  // string to represent a single command
-  //char commands_arr[5];     // array that holds the commands read from shared memory, will set up to be dynamic later
-  //int command_lengths[5];  //temporary, for testing currently
-  //int count = 0;           //temporary, for testing currently
+  int SIZE = 4096;                     // size in bytes of shared memory object
+  char* name = "OS";                   // named of shared memory object
+  int shm_fd;                          // shared memory file descriptor
+  void* mem_ptr;                       // pointer to shared memory object
+  char line[MAX_LINE_LEN];             // string to represent a single command
+  char commands_arr[5][MAX_LINE_LEN];  // array that holds the commands read from shared memory, will set up to be dynamic later
 
-  // create shared memory
-  shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+  int count = 0;  //temporary, for testing currently
 
-  // setup size of shared memory
-  ftruncate(shm_fd, SIZE);
-
-  // map the shared memory
-  mem_ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-
-  pid_t pid = fork();  // first child for reading file/writing to shared memory
+  pid_t pid;
+  pid = fork();  // first child for reading file/writing to shared memory
 
   if (pid < 0) {
     fprintf(stderr, "An error occurred while forking.");
@@ -48,19 +40,20 @@ int main() {
     // child
     FILE* fp = fopen("sample_in.txt", "r");
 
+    // create shared memory
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+    // setup size of shared memory
+    ftruncate(shm_fd, SIZE);
+
+    // map the shared memory
+    mem_ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
     // read from file and write each line to shared memory
     while (fgets(line, MAX_LINE_LEN - 1, fp) != NULL) {
       // printf("line looks like: %s\n", line);
       sprintf(mem_ptr, "%s", line);
-
-      if (line[strlen(line) - 1] == '\n') {
-        mem_ptr += strlen(line) - 2;  // account for end of line
-        //command_lengths[count] = strlen(line) - 2;
-      } else {
-        mem_ptr += strlen(line);
-        //command_lengths[count] = strlen(line);
-      }
-      //count++;
+      mem_ptr += strlen(line);
     }
     fclose(fp);
     close(shm_fd);  // close shared memory
@@ -75,29 +68,43 @@ int main() {
     // map the shared memory
     mem_ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
 
-    // read from shared memory
-    printf("%s", (char*)mem_ptr);
+    // print shared memory result
+    // printf("%s", (char*)mem_ptr);
 
     // remove shared memory object
     shm_unlink(name);
     kill(pid, SIGKILL);  // end child process after done writing to memory
+
+    // split the read memory string into separate command strings
+    count = 0;
+    char* token;
+    const char delimiter[2] = "\n";
+    char shared_memory_result[MAX_LINE_LEN];
+    strcpy(shared_memory_result, mem_ptr);
+    token = strtok(shared_memory_result, delimiter);  // initial starting token (1st command)
+
+    // each token represents a different linux command string, and is added to the array.
+    while (token != NULL) {
+      // printf("\n\ntoken is: %s\n", token);
+      strcpy(commands_arr[count], token);
+      count++;
+      token = strtok(NULL, delimiter);  // move to next token
+    }
+    // ================== testing ==================== //
+    /*
+    printf("\nSplitting completed.\n");
+    for (int i = 0; i < 5; i++) {
+      printf("Command at index %d is: %s\n", i, commands_arr[i]);
+    }
+    */
+
+    // ================== testing =================== //
   }
-
-  // split the shared memory string into separate commands and add them to the array (eventually dynamic)
-  // for (int i = 0; i < 5; i++) {
-  //   char temp[MAX_LINE_LEN];
-  //   //strncpy(temp, line, command_lenghts[i]);
-  //   //commands_arr[i] = line;
-  // }
-
-  // for (int i = 0; i < 5; i++) {
-  //   printf("\ncommand length here is: %d\n", command_lengths[i]);
-  // }
 
   return 0;
 }
 
-char* line = NULL;
+// char* line = NULL;
 // #define SHM_NAME "PROC_SHM"
 // #define MEM_SPACE 1048576
 
